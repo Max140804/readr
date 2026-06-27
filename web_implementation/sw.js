f2445578665d2ddd6523d273ed6768099c0dd6af
Max140804 <nnamani.chideramichael@gmail.com>
2026-06-27 11:22:22 +0100

    l

--- web_implementation/sw.js
+++ web_implementation/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'readr-web-v1';
+const CACHE_NAME = 'readr-web-v3';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -7,6 +7,8 @@ const ASSETS_TO_CACHE = [
     './js/data.js',
     './js/db.js',
     './js/sync-service.js',
+    './assets/logo.png',
+    './manifest.json',
     'https://cdn.tailwindcss.com',
     'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;700;900&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
@@ -19,6 +21,20 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
